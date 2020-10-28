46d37c04d688b3fb1ff243a66459dd1a3a8276ea
Aditya <aditya.aparadh.0@gmail.com>
2020-10-28 16:12:32 +0530

    Added the initial files

---
+++ intro.bat
@@ -0,0 +1,295 @@
+:: Please Adhere to these general guidelines for contibuting code:
+
+
+:: (1) Use smallcase letters for commands. Eg echo Toolkit (✓) ; ECHO Toolkit (✗)
+
+:: (2) Use UPPERCASE letters and UNDERSCORES(_) for naming functions. Eg  :THIS_TOOL (✓) ; :this-Tool- (✗)
+
+:: (3) Please choose the Function names so that they are Relevant to the function. Eg For a Password Generator function, you can name it :PASSWORD_GEN
+
+:: (4) Consider adding useful comments to document the code.
+
+:: (5) As a general pratice use '::' for comments instead of 'REM'. Eg ::This is a comment (✓) ; REM This is a comment (✗)
+
+
+
+
+
+
+
+@echo off
+
+
+:MAIN
+color 0F
+echo ###
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                                        ###
+ping localhost -n 1 >nul
+cls
+
+echo ######
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                                     ######
+ping localhost -n 1 >nul
+cls
+
+
+echo #########
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                                  #########
+ping localhost -n 1 >nul
+cls
+
+echo ############
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                               ############
+ping localhost -n 1 >nul
+cls
+
+echo ###############
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                            ###############
+ping localhost -n 1 >nul
+cls
+
+echo ##################
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                         ##################
+ping localhost -n 1 >nul
+cls
+
+
+echo #####################
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                      #####################
+ping localhost -n 1 >nul
+cls
+
+
+echo #########################
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                  #########################
+ping localhost -n 1 >nul
+cls
+
+
+echo #############################
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo              #############################
+ping localhost -n 1 >nul
+cls
+
+echo #################################
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo          #################################
+ping localhost -n 1 >nul
+cls
+
+
+echo ######################################
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo     ######################################
+ping localhost -n 1 >nul
+cls
+
+
+
+echo #########################################
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo #########################################
+ping localhost -n 1 >nul
+cls
+
+echo #########################################
+echo.                                       ##
+echo.                                       ##
+echo.
+echo.
+echo.
+echo.
+echo ##
+echo ##
+echo #########################################
+ping localhost -n 1 >nul
+cls
+
+
+echo #########################################
+echo.                                       ##
+echo.                                       ##
+echo.                                       ##
+echo                                        ##
+echo ##
+echo ##
+echo ##
+echo ##
+echo #########################################
+ping localhost -n 1 >nul
+cls
+
+echo #########################################
+echo.                                       ##
+echo.                                       ##
+echo ##                                     ##
+echo ##                                     ##
+echo ##                                     ##
+echo ##                                     ##
+echo ##
+echo ##
+echo #########################################
+ping localhost -n 1 >nul
+cls
+
+echo #########################################
+echo ##                                     ##
+echo ##                                     ##
+echo ##                                     ##
+echo ##                                     ##
+echo ##                                     ##
+echo ##                                     ##
+echo ##                                     ##
+echo ##                                     ##
+echo #########################################
+ping localhost -n 2 >nul
+
+cls
+echo #########################################
+echo ##                                     ##
+echo ##                                     ##
+echo ##                                     ##
+echo ##   ^>        TOOLKIT        ^<         ##
+echo ##                                     ##
+echo ##                                     ##
+echo ##                                     ##
+echo ##                                     ##
+echo #########################################
+ping localhost -n 2 >nul
+
+cls
+color a
+echo #########################################
+echo ##                                     ##
+echo ##                                     ##
+echo ##                                     ##
+echo ##         ^>  TOOLKIT  ^<               ##
+echo ##                                     ##
+echo ##                                     ##
+echo ##                                     ##
+echo ##                                     ##
+echo #########################################
+ping localhost -n 2 >nul
+
+
+cls
+echo #########################################
+echo ##                                     ##
+echo ##                                     ##
+echo ##                                     ##
+echo ##         ^>  TOOLKIT  ^<               ##
+echo ##                                     ##
+echo ## ^> github.com\AdityaAparadh\Toolkit  ##
+echo ##                                     ##
+echo ##                                     ##
+echo #########################################
+
+
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+
+
+
+
+
+
+pause
+goto :MAIN
